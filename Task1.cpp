d5b96dccc953ce1289498b8fda88e7039be27d80
Ishita927 <ishitamathur927@gmail.com>
2024-06-14 16:10:31 +0530

    Second Time

--- Task1.cpp
+++ Task1.cpp
@@ -4,26 +4,34 @@ using namespace std;
 
 int main(){
 	int i, Guess_Number, Random_Number , num, num2;
+	char ch='Y';
 	
+	while(ch!='N'){
 	cout<<"Guess a number between 1 to 100 : ";
 	cin>>Guess_Number;
 	
-	for(i=0;i<5;i++){
-		Random_Number=rand()% 100;
-//		cout<< Random_Number <<" ";
-	}
-	
-	if(Random_Number > Guess_Number){
+	Random_Number=rand()% 100;
+		
+	if(Random_Number > Guess_Number)
+	{
 		num=Random_Number - Guess_Number;
-	cout<<"Guess number is less than random number"<< num;	
+	    cout<<"Guess number is less than random number by : "<< num<<"\n";
+	    cout<<"Random Number:"<<Random_Number;	
 	}
-	else if(Random_Number < Guess_Number){
+	else if(Random_Number < Guess_Number)
+	{
     	num2= Guess_Number - Random_Number;
-    	cout<<"Guess number is less than random number"<< num2;	
+    	cout<<"Guess number is more than random number by : "<< num2<<"\n";	
+    	cout<<"Random Number:"<<Random_Number<<"\n";
 	}
 	else{
-	cout<<"Guess number matches the random number";
+	    cout<<"Guess number matches the random number"<<"\n";
+    }
+    cout<<"Do you want to play again Y/N : ";
+    cin>>ch;
+    cout<<"\n";
 }
+   
 return 0;
 	
 }
